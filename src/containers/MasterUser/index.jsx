36d9b7769c4b43aaf90e7fd03ad65752cde7237e
gusnaikhsan <gusna.ikhsan7@gmail.com>
2022-01-05 09:45:47 +0700

    fixing

--- src/containers/MasterUser/index.jsx
+++ src/containers/MasterUser/index.jsx
@@ -9,7 +9,7 @@ const Index = () => (
   <Container className="dashboard">
     <Row>
       <Col md={12}>
-        <h3 className="page-title">Master User</h3>
+        <h3 className="page-title">User</h3>
       </Col>
     </Row>
     <Row>
